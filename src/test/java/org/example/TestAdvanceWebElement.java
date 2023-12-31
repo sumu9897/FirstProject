64dd72d09da88afcd19bc6a60629b1365dc15281
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-12-31 13:43:57 +0600

    Update

---
+++ src/test/java/org/example/TestAdvanceWebElement.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class TestAdvanceWebElement extends DriverSetup{
+
+    @Test
+    public void TestElement() throws InterruptedException {
+        browser.get("https://trytestingthis.netlify.app/");
+
+//        WebElement radioButton1 = browser.findElement(By.cssSelector("input[value='radio1']"));
+//        radioButton1.click();
+//        Thread.sleep(1000);
+//
+//        WebElement selectDropdown = browser.findElement(By.xpath("//select[@id=\"dropdown-class-example\"]"));
+//        selectDropdown.click();
+//        Thread.sleep(1000);
+
+        System.out.println(browser.findElement(By.xpath("//div/h1")).getText());
+    }
+
+}
