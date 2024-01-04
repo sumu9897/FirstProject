b7ff832f8f567c71f8ed8fb5710e236c66c75649
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-04 18:23:01 +0600

    Navigate

--- src/test/java/org/example/GetNavigate.java
+++ src/test/java/org/example/GetNavigate.java
@@ -13,36 +13,37 @@ public class GetNavigate extends DriverSetup{
         System.out.println(browser.getTitle());
         Thread.sleep(1500);
         browser.navigate().back();
-//        System.out.println(browser.getTitle());
-//        Thread.sleep(3000);
-//        browser.navigate().forward();
-//        System.out.println(browser.getTitle());
-//        Thread.sleep(5000);
-//        browser.navigate().refresh();
-//        Thread.sleep(5000);
+        System.out.println(browser.getTitle());
+        Thread.sleep(1000);
+        browser.navigate().forward();
+        System.out.println(browser.getTitle());
+        Thread.sleep(3000);
+        browser.navigate().refresh();
+        Thread.sleep(5000);
+
     }
 
-//    @Test
-//    public void testBrowserWindow() throws InterruptedException {
-//        browser.get("https://trytestingthis.netlify.app");
-//        System.out.println(browser.getTitle());
-//        Thread.sleep(1000);
-//        browser.switchTo().newWindow(WindowType.TAB);
-//        browser.navigate().to ("https://rahulshettyacademy.com/AutomationPractice/");
-//        System.out.println(browser.getTitle());
-//        Thread.sleep(1000);
-//        browser.switchTo().newWindow(WindowType.WINDOW);
-//        browser.navigate().to("https://facebook.com");
-//        System.out.println(browser.getTitle());
-//        Thread.sleep(1000);
-//        browser.manage().window().minimize();
-//        Thread.sleep(3000);
-//        browser.manage().window().maximize();
-//        Thread.sleep(3000);
-//        browser.manage().window().fullscreen();
-//        Thread.sleep(3000);
-//        System.out.println(browser.getCurrentUrl());
-//        browser.close();
-//        browser.quit();
-//    }
+    @Test
+    public void testBrowserWindow() throws InterruptedException {
+        browser.get("https://trytestingthis.netlify.app");
+        System.out.println(browser.getTitle());
+        Thread.sleep(1000);
+        browser.switchTo().newWindow(WindowType.TAB);
+        browser.navigate().to ("https://rahulshettyacademy.com/AutomationPractice/");
+        System.out.println(browser.getTitle());
+        Thread.sleep(1000);
+        browser.switchTo().newWindow(WindowType.WINDOW);
+        browser.navigate().to("https://facebook.com");
+        System.out.println(browser.getTitle());
+        Thread.sleep(1000);
+        browser.manage().window().minimize();
+        Thread.sleep(3000);
+        browser.manage().window().maximize();
+        Thread.sleep(3000);
+        browser.manage().window().fullscreen();
+        Thread.sleep(3000);
+        System.out.println(browser.getCurrentUrl());
+        browser.close();
+        browser.quit();
+  }
 }
