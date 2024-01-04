bf0a0911af16f04ba9f701712b4a7fa3a2dd60f5
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-04 06:54:31 +0600

    InteractingwithWebElement

--- src/test/java/org/example/InteractingWithWebElement.java
+++ src/test/java/org/example/InteractingWithWebElement.java
@@ -1,12 +1,17 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class InteractingWithWebElement extends DriverSetup{
 
     @Test
-    public void  testInteractionofWebElemnet(){
+    public void  testInteractionofWebElemnet() throws InterruptedException {
         browser.get("https://trytestingthis.netlify.app/");
+        WebElement firstNameInputBox = browser.findElement(By.xpath("//input[@id='fname']"));
+        firstNameInputBox.sendKeys("Mohammad");
+        Thread.sleep(1000);
 
     }
 }
