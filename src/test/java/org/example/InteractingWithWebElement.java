15f6ac130e7ed461915d578b2cb720d418a7f834
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-04 10:25:49 +0600

    InteractingwithWebElement

--- src/test/java/org/example/InteractingWithWebElement.java
+++ src/test/java/org/example/InteractingWithWebElement.java
@@ -16,32 +16,49 @@ public class InteractingWithWebElement extends DriverSetup{
         Thread.sleep(3000);
         firstNameInputBox.clear();
         Thread.sleep(1000);
-        String value = browser.findElement(By.xpath("//label[@for='male']")).getAttribute("type");
+        String value = browser.findElement(By.xpath("//input[@id='male']")).getAttribute("value");
         System.out.println(value);
-        String cssValue =  browser.findElement(By.xpath("//label[@for='male']")).getCssValue("font-size");
-        System.out.println(cssValue);
 
-        String elementText =  browser.findElement(By.xpath("//label[@for='male']")).getText();
-        System.out.println(elementText);
+        String type = browser.findElement(By.xpath("//input[@id='female']")).getAttribute("type");
+        System.out.println(type);
 
-        Boolean displayed = browser.findElement(By.xpath("//label[@for='male']")).isDisplayed();
-        System.out.println(displayed);
+        String id = browser.findElement(By.xpath("//input[@id='other']")).getAttribute("id");
+        System.out.println(id);
 
-        browser.get("https://rahulshettyacademy.com/AutomationPractice/");
-        displayed = browser.findElement(By.xpath("//input[@id='displayed-text']")).isDisplayed();
-        System.out.println("Before clicking on the hide button:  " + displayed );
-        browser.findElement(By.xpath("//input[@id='hide-textbox']")).click();
-        displayed = browser.findElement(By.xpath("//input[@id='displayed-text']")).isDisplayed();
-        System.out.println("After clicking on the hide button:  " + displayed );
+        String cssColor =  browser.findElement(By.xpath("//label[@for='male']")).getCssValue("color");
+        System.out.println(cssColor);
 
-        Boolean enable = browser.findElement(By.xpath("//input[@id='displayed-text']")).isEnabled();
-        System.out.println( enable );
+        String cssFont =  browser.findElement(By.xpath("//label[@for='male']")).getCssValue("font-size");
+        System.out.println(cssFont);
 
-        Boolean select = browser.findElement(By.xpath("//input[@id='checkBoxOption1']")).isSelected();
-        System.out.println("Before select: " + select);
-        browser.findElement(By.xpath("//input[@id='checkBoxOption1']")).click();
-        select = browser.findElement(By.xpath("//input[@id='checkBoxOption1']")).isSelected();
-        System.out.println("After select: " + select);
+        String cssMargin =  browser.findElement(By.xpath("//label[@for='female']")).getCssValue("margin");
+        System.out.println(cssMargin);
+
+        String cssPadding =  browser.findElement(By.xpath("//label[@for='female']")).getCssValue("padding");
+        System.out.println(cssPadding);
+
+//
+//        String elementText =  browser.findElement(By.xpath("//label[@for='male']")).getText();
+//        System.out.println(elementText);
+//
+//        Boolean displayed = browser.findElement(By.xpath("//label[@for='male']")).isDisplayed();
+//        System.out.println(displayed);
+//
+//        browser.get("https://rahulshettyacademy.com/AutomationPractice/");
+//        displayed = browser.findElement(By.xpath("//input[@id='displayed-text']")).isDisplayed();
+//        System.out.println("Before clicking on the hide button:  " + displayed );
+//        browser.findElement(By.xpath("//input[@id='hide-textbox']")).click();
+//        displayed = browser.findElement(By.xpath("//input[@id='displayed-text']")).isDisplayed();
+//        System.out.println("After clicking on the hide button:  " + displayed );
+//
+//        Boolean enable = browser.findElement(By.xpath("//input[@id='displayed-text']")).isEnabled();
+//        System.out.println( enable );
+//
+//        Boolean select = browser.findElement(By.xpath("//input[@id='checkBoxOption1']")).isSelected();
+//        System.out.println("Before select: " + select);
+//        browser.findElement(By.xpath("//input[@id='checkBoxOption1']")).click();
+//        select = browser.findElement(By.xpath("//input[@id='checkBoxOption1']")).isSelected();
+//        System.out.println("After select: " + select);
         Thread.sleep(2000);
 
     }
