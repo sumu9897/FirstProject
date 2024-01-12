dacc32cadd5510b153fee11e2c2f07d723205813
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-12 20:09:41 +0600

    HandleSelect

--- src/test/java/org/example/HandleSelectTag.java
+++ src/test/java/org/example/HandleSelectTag.java
@@ -1,4 +1,21 @@
 package org.example;
 
-public class HandleSelectTag {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class HandleSelectTag extends DriverSetup{
+
+    @Test
+    public void testDropDown() throws InterruptedException {
+        browser.get("https://trytestingthis.netlify.app/");
+
+        WebElement dropdown = browser.findElement(By.id("option"));
+        Select select = new Select(dropdown);
+        select.selectByValue("option 1");
+        Thread.sleep(2000);
+
+
+    }
 }
