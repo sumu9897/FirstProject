3e496016676c6a1497e3acaf76a357dcdc21e951
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-12 09:21:51 +0600

    HandelAlert

--- src/test/java/org/example/HandelAlert.java
+++ src/test/java/org/example/HandelAlert.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -9,7 +10,10 @@ public class HandelAlert extends DriverSetup{
     @Test
     public void test_different_alert() throws InterruptedException {
         browser.get("https://the-internet.herokuapp.com/javascript_alerts");
-        browser.findElement(By.xpath("//button[@onclick='jsAlert()']"));
+        browser.findElement(By.xpath("//button[@onclick='jsAlert()']")).click();
+        Alert alert = browser.switchTo().alert();
+        System.out.println(alert.getText());
+
        // Assert.assertEquals(browser.findElement(By.xpath("//p[@id='result']")).getText("You successfully clicked an alert"));
         Thread.sleep(2000);
     }
