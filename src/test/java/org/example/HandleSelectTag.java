aeeefe66d0ffa52e3d41ffdba95f573d370d8532
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-12 20:12:03 +0600

    Select Option

--- src/test/java/org/example/HandleSelectTag.java
+++ src/test/java/org/example/HandleSelectTag.java
@@ -13,8 +13,13 @@ public class HandleSelectTag extends DriverSetup{
 
         WebElement dropdown = browser.findElement(By.id("option"));
         Select select = new Select(dropdown);
-        select.selectByValue("option 1");
+        dropdown.click();
         Thread.sleep(2000);
+        select.selectByIndex(2);
+        System.out.println(select.getFirstSelectedOption().getText());
+        Thread.sleep(2000);
+        //select.selectByValue("option 1");
+
 
 
     }
