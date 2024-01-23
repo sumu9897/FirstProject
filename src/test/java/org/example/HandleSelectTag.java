7a31eec461e1677a183095b9fe987863a6adfe40
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-23 21:23:56 +0600

    TestHoverAction

--- src/test/java/org/example/HandleSelectTag.java
+++ src/test/java/org/example/HandleSelectTag.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
 import org.testng.annotations.Test;
 
 public class HandleSelectTag extends DriverSetup{
